c8343d65966d837bc531c8a2f3e021602635766a
deutranium <29124655+deutranium@users.noreply.github.com>
2021-01-11 20:28:30 +0530

    Added frontend boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RegisterApplicant from "./pages/RegisterApplicant"
+
+
+const App = () => {
+    return (
+        <div>
+            <RegisterApplicant />
+        </div>
+    );
+};
+
+export default App;
---
+++ frontend/src/pages/RegisterApplicant.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RegisterApplicant = () => {
+    return (
+        <>
+        lol
+        </>
+    );
+}
+
+export default RegisterApplicant;
