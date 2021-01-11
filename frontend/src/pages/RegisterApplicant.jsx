cff6ea08521bbeb2616fa569e254e1e8fcb992cd
deutranium <29124655+deutranium@users.noreply.github.com>
2021-01-12 00:57:11 +0530

    Created basic form

--- frontend/src/pages/RegisterApplicant.jsx
+++ frontend/src/pages/RegisterApplicant.jsx
@@ -1,9 +1,34 @@
 import React from "react"
+import ReactDOM from 'react-dom';
+import { FormControl, TextField, Button, InputLabel, Input, InputAdornment } from '@material-ui/core'
+
 
 const RegisterApplicant = () => {
     return (
         <>
-        lol
+            <FormControl>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Email"
+                    variant="outlined"
+                    margin="dense"
+                    fullWidth
+                />
+                <TextField
+                    required
+                    id="outlined-password-input outline-required"
+                    label="Password"
+                    type="Password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                    margin="dense"
+                    fullWidth
+                />
+                <Button variant="contained" color="primary">
+                    Submit
+                </Button>
+            </FormControl>
         </>
     );
 }
