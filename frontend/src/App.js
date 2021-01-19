b376595645f5dc88ef78d3a894ddf612105b41ae
dvijaywargiya <deveshvijaywargiya@Deveshs-MacBook-Pro.local>
2021-01-19 15:41:16 +0530

    Minor change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,11 +13,34 @@ import UserContext from "./context/userContext";
 import "./App.css";
 import { MuiThemeProvider } from "@material-ui/core";
 
+
+const initialState = {
+  token: undefined,
+  user: undefined,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        token: action.payload.token,
+        user: action.payload.userRes.data,
+      };
+    default:
+      return state;
+  }
+};
+
+
+
 function App() {
-  const [userData, setUserData] = useState({
-    token: undefined,
-    user: undefined,
-  });
+  const [userData, changeUserData] = React.useReducer(reducer, initialState);
+
+  // const [userData, setUserData] = useState({
+  //   token: undefined,
+  //   user: undefined,
+  // });
 
   useEffect(() => {
     const checkLoggedIn = async () => {
@@ -40,10 +63,17 @@ function App() {
         console.log(userRes);
 
         // ERROR HEEEEEEEERRRRRRRRREEEEEEEEEE!!!!!!!!
-        setUserData({
-          token,
-          user: userRes.data,
-        });
+        changeUserData({
+          type: "LOGIN",
+          payload: {
+            token,
+            userRes            
+          }
+        })
+        // setUserData({
+        //   token,
+        //   user: userRes.data,
+        // });
         // console.log("1111111");
         // console.log(userData)
       }
@@ -60,7 +90,7 @@ function App() {
       <MuiThemeProvider theme={T.theme}>
         <GlobalStyle />
         <BrowserRouter>
-          <UserContext.Provider value={{ userData, setUserData }}>
+          <UserContext.Provider value={{ userData, changeUserData }}>
             {/* <Landing /> */}
             {/* <Header /> */}
             <Switch>
